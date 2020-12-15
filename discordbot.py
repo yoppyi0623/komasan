5f639376fecd57202272f1155f1e1f180e12a1a8
yoppyi0623 <59519214+yoppyi0623@users.noreply.github.com>
2020-12-15 17:53:11 +0900

    implement uso5

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('ping')
     
     @bot.command()
 async def uso(ctx):
